a4387f0e9958b847d41b29cf8d65ee6c7a862e15
Damian Hickey <dhickey@gmail.com>
2020-11-02 00:12:38 +0100

    Initial template.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run --rm --name lib-build \
+ -v /var/run/docker.sock:/var/run/docker.sock \
+ -v $PWD:/repo \
+ -w /repo \
+ -e FEEDZ_LOGICALITY_API_KEY=$FEEDZ_LOGICALITY_API_KEY \
+ -e BUILD_NUMBER=$GITHUB_RUN_NUMBER \
+ --network host \
+ damianh/dotnet-sdks:2 \
+ dotnet run -p build/Build.csproj -c Release -- "$@"
